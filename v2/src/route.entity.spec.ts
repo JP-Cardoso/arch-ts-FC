a565a4cb74268b8e1d0a01e737bdd01006cba348
JP-Cardoso <jhonpoal@hotmail.com>
2023-06-26 22:56:26 -0300

    feat: teste da entidade

--- v2/src/route.entity.spec.ts
+++ v2/src/route.entity.spec.ts
@@ -1,6 +1,75 @@
+import { LatLng, Route, RoutesProps } from './route.entity';
+
 describe('Route Tests', () => {
 
     test('Constructor', () => {
-        
-    })
+        let routeProps: RoutesProps = {
+            title: 'Minha Rota',
+            startPosition: { lat: 5, lng: 10 },
+            endPosition: { lat: 10, lng: 15 }
+        };
+        let route = new Route(routeProps);
+        expect(route.props).toStrictEqual({
+            ...routeProps, points: []
+        });
+
+        routeProps = {
+            title: 'Minha Rota',
+            startPosition: { lat: 5, lng: 10 },
+            endPosition: { lat: 10, lng: 15 },
+            points: [
+                { lat: 11, lng: 15 }
+            ]
+        };
+
+        route = new Route(routeProps);
+        expect(route.props).toStrictEqual({
+            ...routeProps,
+            points: [
+                { lat: 11, lng: 15 }
+            ]
+        });
+    });
+
+    test('Updated Title Method', () => {
+        let routeProps: RoutesProps = {
+            title: 'Minha Rota',
+            startPosition: { lat: 5, lng: 10 },
+            endPosition: { lat: 10, lng: 15 }
+        };
+        let route = new Route(routeProps);
+
+        route.updateTitle('Nova Rota');
+        expect(route.title).toStrictEqual('Nova Rota');
+    });
+
+    test('Updated Position Method', () => {
+        let routeProps: RoutesProps = {
+            title: 'Minha Rota',
+            startPosition: { lat: 5, lng: 10 },
+            endPosition: { lat: 10, lng: 15 }
+        };
+        let route = new Route(routeProps);
+        const start: LatLng = { lat: 20, lng: 30 };
+        const end: LatLng = { lat: 30, lng: 45 };
+        route.updatedPosition(start, end);
+
+        expect(route.startPosition).toBe(start);
+        expect(route.endPosition).toBe(end);
+    });
+
+    test('Updated Points Method', () => {
+        let routeProps: RoutesProps = {
+            title: 'Minha Rota',
+            startPosition: { lat: 5, lng: 10 },
+            endPosition: { lat: 10, lng: 15 }
+        };
+        let route = new Route(routeProps);
+        const points: LatLng[] = [{ lat: 20, lng: 30 }];
+        route.updatedPoints(points);
+
+        expect(route.points).toHaveLength(1);
+        expect(route.points).toStrictEqual(points);
+    });
+
 })
--- v2/src/route.entity.ts
+++ v2/src/route.entity.ts
@@ -1,23 +1,26 @@
+/**
+ * A Regra de Negócio fica em suas entidades
+ */
 export type LatLng = { lat: number, lng: number };
 
-export type RouteProps = {
+export type RoutesProps = {
     title: string,
     startPosition: LatLng,
     endPosition: LatLng,
-    points: LatLng[],
+    points?: LatLng[],
 };
 
-export default class Route {
-    public props: Required<RouteProps>
+export class Route {
+    public props: Required<RoutesProps>
     constructor(
-        props: RouteProps
+        props: RoutesProps
     ) {
         // this.props = props
         this.props = {
             ...props,
             points: props.points || []
         }
-    }
+    };
 
     get title() {
         return this.props.title
@@ -27,20 +30,42 @@ export default class Route {
         this.props.title = value;
     };
 
+    get startPosition() {
+        return this.props.startPosition;
+    };
+
+    private set startPosition(value: LatLng) {
+        this.props.startPosition = value;
+    };
+
+    get endPosition() {
+        return this.props.endPosition;
+    };
+
+    private set endPosition(value: LatLng) {
+        this.props.endPosition = value;
+    };
+      
+    get points() {
+        return this.props.points
+    }
+
+    private set points(value: LatLng[]) {
+        this.props.points = value;
+    }
+
     updateTitle(value: string) {
         this.title = value;
     };
 
+    updatedPosition(startPosition: LatLng, endPosition: LatLng) {
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+    };
+
+    updatedPoints(value: LatLng[]) {
+        this.points = value;
+    };
+
 };
 
-const rota = new Route(
-    {
-        title: 'Minha rota',
-        startPosition: { lat: 15, lng: 20 },
-        endPosition: { lat: 20, lng: 30 },
-        points: [
-            { lat: 15, lng: 20 },
-            { lat: 20, lng: 30 }
-        ],
-    }
-);
